b018cc21dc88a12be854bb910b8e17425b6e4818
Sanmu <hoso1312@gmail.com>
2018-06-03 21:10:58 +1000

    update facebook link preview

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -3,13 +3,14 @@ import Document, { Head, Main, NextScript } from 'next/document'
 export default class extends Document {
   render() {
     return (
-      <html>
-      
+      <html xmlns="http://www.w3.org/1999/xhtml" xmlnsFb="http://ogp.me/ns/fb#">
+
         <Head>
           <title>What is that is Fun?</title>
           <link rel="stylesheet" href="/_next/static/style.css"/>
           <link rel="icon" href="/static/favicon.ico" type="image/x-icon"/>
           <meta name="viewport" content="width=device-width, initial-scale=0.8, user-scalable=no"/>
+          <meta property="og:image" content="/static/moffat.png"/>
         </Head>
 
         <body>
